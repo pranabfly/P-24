60ef61b1d0fd00b4825ad287814650180feaf2d7
pranabfly <73942914+pranabfly@users.noreply.github.com>
2021-01-29 15:46:02 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+
+
+function setup(){
+    var canvas = createCanvas(1200,600);
+    engine = Engine.create();
+    world = engine.world;
+
+	Hammer1 = new Hammer(200,200,150,50)
+	Hammer2 = new Hammer(300,40,130,30)
+
+    plane1 = new Plane(600,height,1200,20)
+
+    rub1 = new Rubber(900,450,70)
+    
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+	Hammer1.display()
+	Hammer2.display()
+    plane1.display()
+    rub1.display()
+    
+}
